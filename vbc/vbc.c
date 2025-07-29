0313851ca0b4e77025d27e99579b3f4d149c38d9
ekeller- <ekeller-@student.42sp.org.br>
2025-07-29 17:41:03 -0300

    solution included

--- vbc/vbc.c
+++ vbc/vbc.c
@@ -1,50 +1,63 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <ctype.h>
 
+// Tree node definition
 typedef struct node {
-    enum {
-        ADD,
-        MULTI,
-        VAL
-    }   type;
+    enum { ADD, MULTI, VAL } type;
     int val;
-    struct node *l;
-    struct node *r;
-}   node;
+    struct node *l, *r;
+} node;
 
-node    *new_node(node n)
+// Memory-safe node creation
+node *new_node(node n)
 {
-    node *ret = calloc(1, sizeof(n));
-    if (!ret)
-        return (NULL);
-    *ret = n;
-    return (ret);
+    node *out = calloc(1, sizeof(node));
+    if (!out)
+        exit(1);
+    *out = n;
+    return out;
 }
 
-void    destroy_tree(node *n)
+// Tree destructor
+void destroy_tree(node *tree)
 {
-    if (!n)
-        return ;
-    if (n->type != VAL)
+    if (!tree)
+        return;
+    destroy_tree(tree->l);
+    destroy_tree(tree->r);
+    free(tree);
+}
+
+// Evaluate tree recursively
+int eval_tree(node *tree)
+{
+    switch (tree->type)
     {
-        destroy_tree(n->l);
-        destroy_tree(n->r);
+        case ADD:
+            return eval_tree(tree->l) + eval_tree(tree->r);
+        case MULTI:
+            return eval_tree(tree->l) * eval_tree(tree->r);
+        case VAL:
+            return tree->val;
     }
-    free(n);
+    return 0;
 }
 
-void    unexpected(char c)
+// Error handler
+void unexpected(char c)
 {
-    if (c)
-        printf("Unexpected token '%c'\n", c);
+    if (c == '\0')
+        printf("Unexpected end of input\n");
     else
-        printf("Unexpected end of file\n");
+        printf("Unexpected token '%c'\n", c);
+    exit(1);
 }
 
+// Helpers to consume tokens
 int accept(char **s, char c)
 {
-    if (**s)
+    if (**s == c)
     {
         (*s)++;
         return (1);
@@ -52,48 +65,79 @@ int accept(char **s, char c)
     return (0);
 }
 
-int expect(char **s, char c)
+void expect(char **s, char c)
 {
-    if (accept(s, c))
-        return (1);
+    if (!accept(s, c))
+        unexpected(**s);
+}
+
+void expect_end(char **s)
+{
+    if (**s != '\0')
+        unexpected(**s);
+}
+
+// Forward declarations
+node *parse_expr(char **s);
+node *parse_term(char **s);
+node *parse_factor(char **s);
+
+// factor = digit | (expr)
+node *parse_factor(char **s)
+{
+    node *res;
+
+    if (isdigit(**s))
+        return new_node((node){.type = VAL, .val = *(*s)++ - '0'});
+
+    if (accept(s, '('))
+    {
+        res = parse_expr(s);
+        expect(s, ')');
+        return res;
+    }
+
     unexpected(**s);
-    return (0);
+    return NULL;
 }
 
-//...
-
-node    *parse_expr(char *s)
+// term = factor {* factor}
+node *parse_term(char **s)
 {
-    //...
+    node *left = parse_factor(s);
 
-    if (*s) 
+    while (accept(s, '*'))
     {
-        destroy_tree(ret);
-        return (NULL);
+        node *right = parse_factor(s);
+        left = new_node((node){.type = MULTI, .l = left, .r = right});
     }
-    return (ret);
+    return left;
 }
 
-int eval_tree(node *tree)
+// expr = term {+ term}
+node *parse_expr(char **s)
 {
-    switch (tree->type)
+    node *left = parse_term(s);
+
+    while (accept(s, '+'))
     {
-        case ADD:
-            return (eval_tree(tree->l) + eval_tree(tree->r));
-        case MULTI:
-            return (eval_tree(tree->l) * eval_tree(tree->r));
-        case VAL:
-            return (tree->val);
+        node *right = parse_term(s);
+        left = new_node((node){.type = ADD, .l = left, .r = right});
     }
+    return left;
 }
 
-int main(int argc, char **argv)
+// Main entry point
+int main(int ac, char **av)
 {
-    if (argc != 2)
+    if (ac != 2)
         return (1);
-    node *tree = parse_expr(argv[1]);
-    if (!tree)
-        return (1);
-    printf("%d\n", eval_tree(tree));
+
+    char *s = av[1];
+    node *tree = parse_expr(&s);
+    expect_end(&s);
+    int res = eval_tree(tree);
+    printf("%d\n", res);
     destroy_tree(tree);
+    return (0);
 }
